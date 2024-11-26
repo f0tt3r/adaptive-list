630a76f07b72b3c466550fbe99eb87d04fc76721
f0tt3r <dokyo.kun@mail.ru>
2024-11-26 09:04:00 +0100

    Aufgaben a-f

--- src/AdaptiveList.java
+++ src/AdaptiveList.java
@@ -16,11 +16,58 @@ public class AdaptiveList {
         return this.next;
     }
 
-    public setValue(int value) {
+    public void setValue(int value) {
         this.value = value;
     }
 
-    public setNext(AdaptiveList next) {
+    public void setNext(AdaptiveList next) {
         this.next = next;
+    }
+
+    public static AdaptiveList singletonList(int value) {
+        return new AdaptiveList(value, null);
+    }
+
+    public boolean isLast() {
+        return this.next == null;
+    }
+
+    public AdaptiveList prepend(int value) {
+        AdaptiveList newHead = new AdaptiveList(value, this);
+        return newHead;
+    }
+
+    public AdaptiveList append(int value) {
+        if (this.next == null) {
+            this.next = new AdaptiveList(value, null);
+        } else {
+            this.next.append(value);
+        }
+        return this;
+    }
+
+    public boolean contains(int value) {
+        if (this.value == value) {
+            return true;
+        } else if (this.next != null) {
+            return this.next.contains(value);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean containsAdaptive(int value) {
+        if (this.next == null) {
+            return false;
+        }
+
+        if (this.next.value == value) {
+            AdaptiveList temp = this.next;
+            this.next = temp.next;
+            temp.next = this;
+            return true;
+        } else {
+            return this.next.containsAdaptive(value);
+        }
     }
 }
